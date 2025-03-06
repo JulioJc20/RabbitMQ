dd2874b50f61aeb9b33c42d23b462a6c16b7d6e4
Julio Cesar <juliocesarnascimento199@gmail.com>
2025-03-06 12:12:35 -0300

    Publicador

---
+++ Publicador/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+
+var factory = new ConnectionFactory {HostName = "localhost"};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+
+channel.QueueDeclare(queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    arguments: null);
+
+Console.WriteLine(value: "Digite sua mensagem e aperte <ENTER>");
+
+
+while (true)
+{
+    string message = Console.ReadLine();
+
+    if (message == "")
+        break;
+
+    // var aluno = new Aluno() { Id = 1, Nome = "Milton" };
+    //message = JsonSerializer.Serialize(aluno);
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: string.Empty,          
+                         routingKey:"hello",
+                         basicProperties: null,
+                         body:body);
+
+    Console.WriteLine($"[x] Enviado {message}");
+}
